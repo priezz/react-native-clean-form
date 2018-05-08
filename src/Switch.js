c5988630d54f619ab0e24bbf00d89c61dc93701a
Alexandr Priezzhev <priezz@users.noreply.github.com>
2018-05-08 21:15:46 +0200

    Accept styling for Switch

--- src/Switch.js
+++ src/Switch.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { Switch as BaseSwitch, View } from 'react-native'
-import styled from 'styled-components'
+import {Switch as BaseSwitch} from 'react-native'
 import defaultTheme from './Theme'
 
-const Switch = styled(BaseSwitch)`
-`
+export default class Switch extends React.PureComponent {
+    static defaultProps = {
+        ...BaseSwitch.defaultProps,
+        theme: defaultTheme,
+    }
 
-Switch.propTypes = BaseSwitch.propTypes
+    static propTypes = BaseSwitch.propTypes
 
-Switch.defaultProps = Object.assign({}, BaseSwitch.defaultProps, {
-  theme: defaultTheme,
-  componentName: 'Switch',
-})
-
-export default Switch
+    render() {
+        const {Switch: switchTheme = {}} = this.props.theme
+        return <BaseSwitch
+            {...this.props}
+            onTintColor={switchTheme.onTintColor}
+            thumbTintColor={switchTheme.thumbTintColor}
+            tintColor={switchTheme.tintColor}
+        />
+    }
+}
