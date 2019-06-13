f7a045c416650b70a1c469369d5efb504c8e1d7f
Alexandr Priezzhev <a@priezz.net>
2019-06-13 08:49:12 +0200

    Final update for Input component

--- src/Input.js
+++ src/Input.js
@@ -9,13 +9,11 @@ import defaultTheme from './Theme'
  * @param {Object} props
  * @returns {number}
  */
-const calculateFlexValue = ({ inlineLabel, multiline, numberOfLines }) => {
-  let flex = 1
-  if (multiline && numberOfLines > 0) flex = numberOfLines + 1
-  if (inlineLabel) flex = 0.5
-
-  return flex
-}
+const calculateFlexValue = ({ inlineLabel, multiline, numberOfLines }) => inlineLabel
+  ? 0.5
+  : multiline && numberOfLines > 0
+    ? numberOfLines + 1
+    : 1
 
 /**
  * Decide how the text should be aligned for the input. This decision is based upon
@@ -24,7 +22,7 @@ const calculateFlexValue = ({ inlineLabel, multiline, numberOfLines }) => {
  * @param {Object} props
  * @returns {string}
  */
-const determineTextOrientation = ({ multiline, numberOfLines }) => {
+const determineTextAlignment = ({ multiline, numberOfLines }) => {
   let orientation = 'center'
   if (multiline && numberOfLines > 1) orientation = 'top'
 
@@ -33,49 +31,23 @@ const determineTextOrientation = ({ multiline, numberOfLines }) => {
 
 class Input extends React.Component {
   render() {
-    const { inlineLabel, multiline, numberOfLines, theme = defaultTheme, ...rest } = this.props
+    const { inlineLabel = true, multiline, numberOfLines, theme = defaultTheme, ...rest } = this.props
     console.log('[Input/render]', this.props)
-    return (
-      // <View
-      //   style={{
-      //     backgroundColor: 'green',
-      //     flex: calculateFlexValue({ inlineLabel, multiline, numberOfLines }),
-      //     // height: Platform.OS == 'android' ? theme.BaseInput.lineHeight * 2 : theme.BaseInput.lineHeight,
-      //     // height: 40,
-      //     justifyContent: 'center',
-      //     padding: 0,
-      //   }}
-      // >
-      <TextInput
-        allowFontScaling
-        numberOfLines={numberOfLines}
-        placeholderTextColor={this.props.theme.BaseInput.placeholderColor}
-        style={{
-          color: theme.Input.color,
-          // flex: inlineLabel ? .5 : 1,
-          flex: calculateFlexValue({ inlineLabel, multiline, numberOfLines }),
-          fontSize: theme.BaseInput.fontSize,
-          // height: 40,
-          height: Platform.OS == 'android' ? theme.BaseInput.lineHeight * 2 : theme.BaseInput.lineHeight,
-          lineHeight: theme.BaseInput.lineHeight,
-          textAlignVertical: determineTextOrientation({ multiline, numberOfLines }),
-        }}
-        {...rest}
-      />
-      // </View>
-    )
+    return <TextInput
+      allowFontScaling
+      numberOfLines={numberOfLines}
+      placeholderTextColor={this.props.theme.BaseInput.placeholderColor}
+      style={{
+        color: theme.Input.color,
+        flex: calculateFlexValue({ inlineLabel, multiline, numberOfLines }),
+        fontSize: theme.BaseInput.fontSize,
+        height: Platform.OS == 'android' ? theme.BaseInput.lineHeight * 2 : theme.BaseInput.lineHeight,
+        lineHeight: theme.BaseInput.lineHeight,
+        textAlignVertical: determineTextAlignment({ multiline, numberOfLines }),
+      }}
+      {...rest}
+    />
   }
-}
-
-// Input.propTypes = {
-//   ...TextInput.propTypes,
-//   inlineLabel: PropTypes.bool.isRequired
-// }
-
-Input.defaultProps = {
-  componentName: 'Input',
-  inlineLabel: true,
-  theme: defaultTheme
 }
 
 export default Input
